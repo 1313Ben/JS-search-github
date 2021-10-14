e6524e8711a2ebc4889e1da573d6d92b2d9eb734
Ravindra Benadict Asirvatham <ravindra_github@bluewin.ch>
2021-10-14 18:12:29 +0200

    Added localstorage, users can be saved in localstorage

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import CardList from "./CardList";
 
 class App {
   constructor() {
-    this.cards = [];
+    this.cards = this.getCards();
     this.addCard = this.addCard.bind(this); 
     this.clearCards = this.clearCards.bind(this);
     
@@ -12,10 +12,19 @@ class App {
     this.cards = [data, ...this.cards]; // last entry comes to first
     //console.log(this.cards);
     CardList(this.cards);
+    localStorage.setItem('users', JSON.stringify(this.cards));
   }
   clearCards() {
     this.cards = [];
     CardList(this.cards);
+    localStorage.setItem('users', "");
+  }
+  getCards() {
+    if (localStorage.getItem('users')) {
+      return JSON.parse(localStorage.getItem('users'));
+    } else {
+      return [];
+    }
   }
 }
 
@@ -23,3 +32,5 @@ const app = new App();
 const form = new Form(app.addCard, app.clearCards);
 
 export const render = (html, node) => (node.innerHTML = html);
+
+CardList(app.cards);
